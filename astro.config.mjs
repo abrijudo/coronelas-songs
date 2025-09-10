178c2b849e1aa334ff51fb10f9b1028ecf04102d
abrijudo <101757254+abrijudo@users.noreply.github.com>
2025-09-10 23:06:43 +0200

    fix: vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   adapter: vercel(),
   vite: {
     resolve: {
       alias: {
-        '@db': '/src/db',
+        '@db': fileURLToPath(new URL('./src/db', import.meta.url)),
       },
     },
   },
