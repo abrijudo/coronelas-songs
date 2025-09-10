ca140e1e85c40cb299c67d17db9c522e9c885d8d
Abraham <abrahamsenshi@gmail.com>
2025-09-10 22:49:32 +0200

    asdas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  // ...
   adapter: vercel(),
+  vite: {
+    resolve: {
+      alias: {
+        '@db': '/src/db',
+      },
+    },
+  },
 });
